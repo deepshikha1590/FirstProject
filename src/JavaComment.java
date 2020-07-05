44c7801197923a2a2cb95356e53538432277f30e
Deepshikha <meshikha15@gmail.com>
2020-07-05 20:11:57 +0200

    MyFirstCommit

---
+++ src/JavaComment.java
@@ -0,0 +1,15 @@
+
+public class JavaComment {
+		  public static void main(String[] args) {
+		    /* The code below will print the words Hello World
+		    to the screen, and it is amazing */
+		    System.out.println("Hello World");
+		    
+		    // This is a comment
+		    System.out.println("Hello World, Again");
+		    
+		   
+		    System.out.println("Bye World"); // This is a comment
+		    
+		}
+}
